78343191ecb64303877ad980f9e4a640eea60e5b
ebowwa <basedebowwa@gmail.com>
2024-12-27 14:04:37 -0800

    Initial commit for sketchfab_download

---
+++ main.py
@@ -0,0 +1,26 @@
+from sketchfab_dl import set_api_token, download_model, search_results
+
+# You can find your Sketchfab API Token at:
+# https://sketchfab.com/settings/password
+API_TOKEN = "KEY HERE"
+
+# Search parameters,
+# see: https://docs.sketchfab.com/data-api/v3/index.html#/search
+params = {
+	"type": "models",
+	"q": "cybertruck", 
+	"downloadable": True,
+	"count": 1
+}
+
+# Get a collection of models from the search API
+models = search_results(params)
+if len(models) == 0:
+    print("No models found")
+    exit()
+
+# Downloading requires authentication
+set_api_token(API_TOKEN)
+
+# Download a model with UID to a folder
+download_model(models[0]["uid"], "./cybertruck")
